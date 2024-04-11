a61918f5d855aa88911d9a510a28b9f11edb86ca
Fexas <liuwanting830@outlook.com>
2024-04-11 16:20:53 +0100

    initial commit

---
+++ gcd.py
@@ -0,0 +1,14 @@
+#this class is about the euclideanalgorithm
+class EuclideanAlgorithm:
+    #in this function calculate_gcd, we calculate gce between two number according to the algorithm
+    #gcd(a,b)=gcd(b,a%b) gcd(b, 0) = b
+    #repeat this until the remainder =0, then we find the gcd
+    def calculate_gcd(self, a, b):
+        while b != 0:
+            remainder = a % b
+            a = b#a->b
+            b = remainder#b->remainder
+        return a
+
+a =  EuclideanAlgorithm()
+print(a.calculate_gcd(10,20))
