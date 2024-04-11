1a5ccd5908ab3f011db57d2ecab4bc502ad970eb
Fexas <liuwanting830@outlook.com>
2024-04-11 21:06:16 +0100

    Refactor

--- gcd.py
+++ gcd.py
@@ -1,15 +1,22 @@
-#this class is about the euclideanalgorithm
-class EuclideanAlgorithm:
-    #in this function calculate_gcd, we calculate gce between two number according to the algorithm
-    #gcd(a,b)=gcd(b,a%b) gcd(b, 0) = b
-    #repeat this until the remainder =0, then we find the gcd
-    def calculate_gcd(self, a, b):
-        while b != 0:
-            remainder = a % b
-            a = b#a->b
-            b = remainder#b->remainder
-        return a
+#in this function calculate_gcd, we calculate gce between two number according to the algorithm
+#gcd(a,b)=gcd(b,a%b) gcd(b, 0) = b
+#repeat this until the remainder =0, then we find the gcd
+def calculate_gcd(a, b):
+    while b != 0:
+        remainder = a % b
+        a = b#a->b
+        b = remainder#b->remainder
+    return a
 
-a =  EuclideanAlgorithm()
-print(a.calculate_gcd(10,20))
+#this is a input function, only avaiable for positive number
+def get_number():
+    while True:
+        a = int(input("Enter a non-negative number:"))
+        b = int(input("Enter a non-negative number:"))
+        if a < 0 or b < 0:
+            print("Invalid number")#give error message
+        else:
+            print(f"GCD of {a} and {b} is {calculate_gcd(a, b)}")
+            break
 
+get_number()
